53278b80530506c2cff3811c80e6ddffa16144f5
Eduardo Pariona <epariona.sistemas@gmail.com>
2023-06-03 17:44:46 -0500

    Actualizacion de SQL nueva estructura enviada por Giovanni

--- SQL/Script.sql
+++ SQL/Script.sql
@@ -1,7 +1,7 @@
 USE [SOLUM_EXPRESS]
 GO
 insert into "@EX_PROCESO" (Code,Name,Type,State) values ('SAP_PER_TRACKING','ENVIO DE DATOS DE INTERFACES TRACKING A TABLAS PERIFERICAS',0,0)
-insert into "@EX_MODELO" (Code,Name,SQL,Columnas,State) values ('M3_TRACKING','Modelo consulta de tracking para cliente ARTI','BZ_CONEP_TRACKING_ARTI @A_CLIENTE @A_FECINI @A_FECFIN @A_PEDIDO',22,0)
+insert into "@EX_MODELO" (Code,Name,SQL,Columnas,State) values ('M3_TRACKING','Modelo consulta de tracking para cliente ARTI','BZ_CONEP_TRACKING_ARTI @A_CLIENTE @A_FECINI @A_FECFIN @A_PEDIDO',23,0)
 insert into "@EX_RUTA" (Code,U_EX_CLIENTE,U_EX_PROCESO,U_EX_MODELO,Environment,Server,Port,Route,Sftp,Historic,UserSap,Password,Email,Subject,Attached,Zip,State,LogErr)
 values (144,'20100310288','SAP_PER_TRACKING',NULL,0,NULL,NULL,'C:\FTP Clientes\Arti\IN\TRACKING\TEST\',0,'C:\FTP Clientes\Arti\IN\TRACKING\TEST\HIS\',NULL,NULL,0,NULL,0,0,0,'C:\FTP Clientes\Arti\IN\TRACKING\TEST\LOG\')
 
@@ -13,6 +13,7 @@ Sociedad nvarchar(100) not null,
 HostGroupId varchar(25) not null,
 U_EX_CLIENTE nvarchar(30) not null,
 Interno nvarchar(25) not null,
+FecInterno nvarchar(25) not null,
 Pedido nvarchar(25) not null,
 FecPed nvarchar(25) null,
 FecApruCred nvarchar(25) null,
@@ -27,6 +28,7 @@ Code int identity(1,1) not null,
 Sociedad nvarchar(100) not null,
 U_EX_CLIENTE nvarchar(30) not null,
 Interno nvarchar(25) not null,
+FecInterno nvarchar(25) not null,
 Pedido nvarchar(25) not null,
 FecPed nvarchar(25) null,
 FecApruCred nvarchar(25) null,
@@ -49,8 +51,8 @@ create procedure [dbo].BZ_CONEP_TRACKING_ARTI (
 )      
 as    
 begin transaction transaction_tracking
-insert SAP_TRACKING (Sociedad,U_EX_CLIENTE,Interno,Pedido,FecPed,FecApruCred,FecApruDscto,FecApruExhib,Fact_BV,FecFact_BV,Actualizaciones,Fecha_creacion)
-select t0.Sociedad,'C'+ t0.U_EX_CLIENTE,t0.Interno,t0.Pedido,t0.FecPed,t0.FecApruCred,t0.FecApruDscto,t0.FecApruExhib,t0.Fact_BV,t0.FecFact_BV,0,GETDATE() from SAP_HOST_TRACKING t0
+insert SAP_TRACKING (Sociedad,U_EX_CLIENTE,Interno,FecInterno,Pedido,FecPed,FecApruCred,FecApruDscto,FecApruExhib,Fact_BV,FecFact_BV,Actualizaciones,Fecha_creacion)
+select t0.Sociedad,'C'+ t0.U_EX_CLIENTE,t0.Interno,t0.FecInterno,t0.Pedido,t0.FecPed,t0.FecApruCred,t0.FecApruDscto,t0.FecApruExhib,t0.Fact_BV,t0.FecFact_BV,0,GETDATE() from SAP_HOST_TRACKING t0
 left join SAP_TRACKING T1 on t0.Interno = T1.Interno and t0.Pedido = T1.Pedido
 where HostGroupId = @HostGroupId and (T1.Interno is null and T1.Pedido is null)
 
@@ -71,9 +73,9 @@ go
 CREATE procedure [dbo].[BZ_CONEP_TRACKING_ARTI]
 @A_CLIENTE nvarchar(30),@A_FECINI nvarchar(10),@A_FECFIN nvarchar(10),@A_PEDIDO nvarchar(30)
 as
-select '','N°PEDIDO.ORIGINAL','PEDIDO.ORIGINAL','APROB.CREDITO','APROB.DESCUENTO','APROB.EXHIBIDOR','PEDIDO','CLIENTE','FECHA CREACION','RECIBIDO','PREPARADO','DESPACHADO','ENTREGADO','LIQUIDADO','UBIGEO','TRANSP','PLACA','BULTOS','PESO','GUIA','N°FACTURA','FACTURA/BOLETA','OBS.'
+select '','N°PEDIDO ORIGINAL','PEDIDO ORIGINAL','APROB. CREDITO','APROB. DESCUENTO','APROB. EXHIBIDOR','PEDIDO','CLIENTE','FECHA CREACION','RECIBIDO','PREPARADO','DESPACHADO','ENTREGADO','LIQUIDADO','UBIGEO','TRANSP','PLACA','BULTOS','PESO','GUIA','N°FACTURA','FACTURA BOLETA','OBS.'
 SELECT (select case when (sum(DelivrdQty) <> sum(Quantity)) and convert(CHAR(10),D.U_BZ_FECHA,126) <> ''  then '*' else '' END from RDR1 where RDR1.DocEntry = R.DocEntry) AS 'ESTADO',
-T4.Interno,T4.Pedido,T4.FecApruCred,T4.FecApruDscto,T4.FecApruExhib,
+T4.Pedido,T4.FecInterno,T4.FecApruCred,T4.FecApruDscto,T4.FecApruExhib,
 R.NumAtCard AS PEDIDO,R.U_BZ_CODCLI + '-' + r.U_BZ_CLIENTE AS 'Cliente',T4.FecPed,
 convert(CHAR(10),R.DocDate,126) + ' ' + dbo.GetTime(R.DocTime,0) 'RECIBIDO',
 CASE when R.u_EstadoPacking = 4 then R.U_BZ_CONTAB else '' end 'PREPARADO',
