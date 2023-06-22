e09173d293e15c07fea6bfd3148b59482f953834
Eduardo Pariona <epariona.sistemas@gmail.com>
2023-06-22 10:50:25 -0500

    Fix de dos campos de SUNAT

--- SQL/Script.sql
+++ SQL/Script.sql
@@ -1,7 +1,8 @@
 USE [SOLUM_EXPRESS]
 GO
 insert into "@EX_PROCESO" (Code,Name,Type,State) values ('SAP_PER_TRACKING','ENVIO DE DATOS DE INTERFACES TRACKING A TABLAS PERIFERICAS',0,0)
-insert into "@EX_MODELO" (Code,Name,SQL,Columnas,State) values ('M3_TRACKING','Modelo consulta de tracking para cliente ARTI','BZ_CONEP_TRACKING_ARTI @A_CLIENTE @A_FECINI @A_FECFIN @A_PEDIDO',23,0)
+insert into "@EX_MODELO" (Code,Name,SQL,Columnas,State) values ('M3_TRACKING','Modelo consulta de tracking para cliente ARTI','BZ_CONEP_TRACKING_ARTI @A_CLIENTE @A_FECINI @A_FECFIN @A_PEDIDO',25,0)
+--delete "@EX_MODELO" where Code = 'M3_TRACKING'
 insert into "@EX_RUTA" (Code,U_EX_CLIENTE,U_EX_PROCESO,U_EX_MODELO,Environment,Server,Port,Route,Sftp,Historic,UserSap,Password,Email,Subject,Attached,Zip,State,LogErr)
 values (144,'20100310288','SAP_PER_TRACKING',NULL,0,NULL,NULL,'C:\FTP Clientes\Arti\IN\TRACKING\TEST\',0,'C:\FTP Clientes\Arti\IN\TRACKING\TEST\HIS\',NULL,NULL,0,NULL,0,0,0,'C:\FTP Clientes\Arti\IN\TRACKING\TEST\LOG\')
 
@@ -40,7 +41,7 @@ Actualizaciones int not null,
 Fecha_creacion datetime not null,
 Fecha_actualizacion datetime null
 )
-
+--select * from SAP_TRACKING
 IF EXISTS (SELECT * FROM sysobjects WHERE name='BZ_CONEP_TRACKING_ARTI') 
 BEGIN
 	drop procedure [dbo].BZ_CONEP_TRACKING_ARTI
@@ -73,7 +74,7 @@ go
 CREATE procedure [dbo].[BZ_CONEP_TRACKING_ARTI]
 @A_CLIENTE nvarchar(30),@A_FECINI nvarchar(10),@A_FECFIN nvarchar(10),@A_PEDIDO nvarchar(30)
 as
-select 'PEDDIFPICKING','N°PEDIDO ORIGINAL','PEDIDO ORIGINAL','APROB. CREDITO','APROB. DESCUENTO','APROB. EXHIBIDOR','PEDIDO','CLIENTE','FECHA CREACION','RECIBIDO','PREPARADO','DESPACHADO','ENTREGADO','LIQUIDADO','UBIGEO','TRANSP','PLACA','BULTOS','PESO','GUIA','N° FACTURA','FACTURA BOLETA','OBS.'
+select 'PEDDIF PICKING','N°PEDIDO ORIGINAL','PEDIDO ORIGINAL','APROB. CREDITO','APROB. DESCUENTO','APROB. EXHIBIDOR','PEDIDO','CLIENTE','FECHA CREACION','RECIBIDO','PREPARADO','DESPACHADO','ENTREGADO','LIQUIDADO','UBIGEO','TRANSP','PLACA','BULTOS','PESO','GUIA','GUIA REMISION','GUIA REMISION SUNAT','N° FACTURA','FACTURA BOLETA','OBS.'
 SELECT (select case when (sum(DelivrdQty) <> sum(Quantity)) and convert(CHAR(10),D.U_BZ_FECHA,126) <> ''  then '*' else '' END from RDR1 where RDR1.DocEntry = R.DocEntry) AS 'ESTADO',
 T4.Pedido,T4.FecPed,T4.FecApruCred,T4.FecApruDscto,T4.FecApruExhib,
 R.NumAtCard AS PEDIDO,R.U_BZ_CODCLI + '-' + r.U_BZ_CLIENTE AS 'Cliente',T4.FecInterno,
@@ -82,15 +83,17 @@ CASE when R.u_EstadoPacking = 4 then R.U_BZ_CONTAB else '' end 'PREPARADO',
 convert(CHAR(10),N.U_BZ_FFINAL,126) AS 'ENVIADO',
 convert(CHAR(10),N.U_BZ_TCKFEEN,126) + ' ' + dbo.GetTime(N.U_BZ_TCKHOEN,0) 'ENTREGADO',
 convert(CHAR(10),N.U_BZ_TCKFELQ,126) + ' ' + dbo.GetTime(N.U_BZ_TCKHOLQ,0) 'LIQUIDADO',
-R.Address2 AS 'UBIGEO',N.U_BZ_TRANSPORTISTA 'TRANSP',N.U_BZ_PLACA 'PLACA',N.U_BZ_BULTOS 'BULTOS',N.U_BZ_PESO_KG 'PESO',N.U_SYP_MDSD + '-' + N.U_SYP_MDCD 'GUIA',T4.Fact_BV,T4.FecFact_BV,R.U_BZ_EVENTUALIDADES 'OBS.'
+R.Address2 AS 'UBIGEO',N.U_BZ_TRANSPORTISTA 'TRANSP',N.U_BZ_PLACA 'PLACA',N.U_BZ_BULTOS 'BULTOS',N.U_BZ_PESO_KG 'PESO',N.U_SYP_MDSD + '-' + N.U_SYP_MDCD 'GUIA',convert(varchar(16),N.Docdate + ' ' + dbo.GetTime(N.DocTime,0),120) 'GUIAREMISION',T5.bl_fechaRespuestaSUNAT,T4.Fact_BV,T4.FecFact_BV,R.U_BZ_EVENTUALIDADES 'OBS.'
 FROM ORDR R LEFT JOIN ODLN N ON R.CardCode = N.CardCode AND R.NumAtCard = N.NumAtCard LEFT JOIN [@BZ_CARGA] C ON R.U_BZ_CARGA = C.Code LEFT JOIN [@BZ_DETCARGA] D ON D.Code = C.Code
 LEFT JOIN SOLUM_EXPRESS.dbo.SAP_TRACKING T4 ON T4.U_EX_CLIENTE COLLATE DATABASE_DEFAULT = R.CardCode and T4.Interno COLLATE DATABASE_DEFAULT = R.NumAtCard
+LEFT JOIN [BIZLINKS_PRD].dbo.SPE_DESPATCH_RESPONSE T5 ON T5.serieNumeroGuia = (N.U_SYP_MDSD + '-' + N.U_SYP_MDCD)
 WHERE R.CardCode = @A_CLIENTE AND R.CANCELED <> 'Y' AND RIGHT(r.NumAtCard,9) <> 'CANCELADO' AND isnull(N.CANCELED,'') NOT IN ('Y','C') AND R.NumAtCard like
 	case  
 		when @A_PEDIDO = '' OR ISNULL(@A_PEDIDO,'') = '' then  '%%' else @A_PEDIDO 
 	end
 AND R.DocDate BETWEEN (isnull(@A_FECINI,convert(CHAR(10), dateadd(day,-31,getdate()),126))) AND isnull(@A_FECFIN,convert(CHAR(10),getdate(),126)) order by 4 asc
-SELECT '','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','N','N','A','A','A','A'
-SELECT '','R','R','R','R','R','R','R','R','R','R','R','R','R','R','','','','','','','',''
+SELECT '','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','N','N','A','A','A','A','A','A'
+SELECT '','R','R','R','R','R','R','R','R','R','R','R','R','R','R','','','','','','','','','',''
 GO
 
+--select * from [BIZLINKS_PRD].dbo.SPE_DESPATCH_RESPONSE where serieNumeroGuia = 'TR07-00005647'
